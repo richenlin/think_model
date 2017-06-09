12587c4c0b499fef12f67d9e22e7749ba0f02ea4
yanglin <richenlin@gmail.com>
2017-06-09 23:21:51 +0800

    think.model方法移植

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * @license    MIT
  * @version    17/6/6
  */
+const lib = require('think_lib');
 const orm = require('thinkorm');
 /*eslint-disable consistent-return */
 module.exports = function (options) {
@@ -19,7 +20,32 @@ module.exports = function (options) {
         for (n in models) {
             ps.push(orm.setCollection(models[n], options));
         }
-        return Promise.all(ps);
+        return Promise.all(ps).then(() => {
+            think.model = function (name, config) {
+                try {
+                    let cls;
+                    if (!lib.isString(name) && name.__filename) {
+                        cls = lib.require(name.__filename);
+                    } else if (think._caches.models[name]) {
+                        cls = think._caches.models[name];
+                    }
+                    if (!cls) {
+                        return think.log(`Model ${name} is undefined`, 'ERROR');
+                    }
+                    if (config === undefined) {
+                        return cls;
+                    }
+                    config = lib.extend(options || {}, config);
+                    //print sql
+                    config.db_ext_config && (config.db_ext_config.db_log_sql = think.app_debug || false);
+                    return new cls(config || {});
+                } catch (e) {
+                    think.log(e);
+                    return null;
+                }
+            };
+            return;
+        });
     });
 
     return function (ctx, next) {
